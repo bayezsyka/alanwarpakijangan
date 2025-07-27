a2c8ab2b9d8117d1d5aae6e3798b6daa0001fcbb
bayezsyka <farrosy6@gmail.com>
2025-07-27 21:22:18 +0700

    perbarui head

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Pondok Pesantren Al-Anwar Pakijangan</title>
     <meta name="description" content="Selamat datang di website resmi Pondok Pesantren Al-Anwar Pakijangan. Kami menyatukan pendidikan berkualitas dengan nilai-nilai Islam untuk membentuk generasi berkarakter. Informasi pendaftaran santri baru (PSB)."> 
-    <title>@yield('title', 'Pesantren Al-Anwar')</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
