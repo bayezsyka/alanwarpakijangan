96ec4c1fa626d9e0bebfe34dcacfdb38d897c632
ezarwiratama <ezar.hardin10@gmail.com>
2025-08-06 20:30:00 +0700

    fix bug button back on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,8 +74,7 @@
         <div class="flex items-center justify-between mt-6">
             <a
                 href="{{ url()->previous() }}"
-                class="inline-flex items-center px-4 py-2 text-white text-sm font-semibold rounded-full hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 transition"
-                style="background-color: #008362;"
+                class="inline-flex items-center px-4 py-2 bg-[#008362] text-white text-sm font-semibold rounded-full hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-600 transition"
             >
                 <svg
                     class="w-4 h-4 me-2"
@@ -92,7 +91,6 @@
                 </svg>
                 Kembali
             </a>
-        </div>
             <div class="flex items-center space-x-3">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
